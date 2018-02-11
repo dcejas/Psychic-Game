db4c226ae7e1af8e5811e756a7d5b62be8feac09
David Cejas <dcejas430@gmail.com>
2018-02-11 14:18:20 -0500

    "Adding 1st JS code"

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,23 @@
 /* JS PROPERLY LINKED */
-/* Testing VS Code built-in GIT Source Control feature */
+var alphabet = ['abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('')];
+
+document.onkeyup = function(event)
+    {
+        var userGuess = event.key;
+
+        var computerGuess = alphabet[Math.floor(Math.random() *alphabet.length)];
+
+        if(userGuess === "a" && computerGuess === "p"){
+            loses++;
+        }
+        alert("user guess: " + userGuess);
+
+        alert("Computer guess: "+ computerGuess);
+
+
+        document.getElementById("wins").innerHTML="Wins:" + wins;
+        document.getElementById("losses").innerHTML="Losses:" + losses;
+        document.getElementById("guessesLeft").innerHTML="Guesses Left:" + guessesLeft;
+        document.getElementById("guessesSoFar").innerHTML="Your Guesses so Far:" + guessesSoFar;
+
+    };
