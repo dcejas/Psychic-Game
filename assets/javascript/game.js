38bf5141fa48f0fd70c774c9858f00013cedbc04
David Cejas <dcejas430@gmail.com>
2018-02-11 15:57:08 -0500

    Cleaned up code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,8 +21,8 @@ document.onkeyup = function(event) {
         else if (guessesLeft ===0){
             losses++;
             alert("You didn't guess the correct letter. You lost. Let's try again.");
-            guessesLeft = 9; //reseting the guesses back to 9 so that the user can play again
-            guessesSoFar.length = 0; //this removes everything from the guesses so far array, so that the guesses from the previous round don't show  
+            guessesLeft = 9; 
+            guessesSoFar.length = 0; 
         }
 
         else if (userGuess !== computerGuess) {
@@ -49,13 +49,4 @@ document.onkeyup = function(event) {
 
         // Placing the html into the game ID
         document.querySelector('#game').innerHTML = html;
-
-        // alert("user guess: " + userGuess);
-        // alert("Computer guess: "+ computerGuess);
-
-        // document.getElementById("wins").innerHTML="Wins:" + wins;
-        // document.getElementById("losses").innerHTML="Losses:" + losses;
-        // document.getElementById("guessesLeft").innerHTML="Guesses Left:" + guessesLeft;
-        // document.getElementById("guessesSoFar").innerHTML="Your Guesses so Far:" + guessesSoFar;
-
     };
